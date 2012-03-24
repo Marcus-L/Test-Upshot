f0d457082ade426fed8a34e412607d669e23f7df
MarcusL <marcusl@gmail.com>
2012-03-24 16:37:25 -0400

    Add Test Files

---
+++ TestApp/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Data.EntityFramework;
+
+namespace TestApp.Controllers
+{
+	public class TestController : DbDataController<TestEntities>
+    {
+		public IQueryable<ParentThing> GetParentThings()
+		{
+			return DbContext.ParentThings.Include("Children");
+		}
+
+		public HttpResponseMessage ReplaceThings()
+		{
+			using (var context = new TestEntities())
+			{
+				var firstParent = context.ParentThings.Include("Children").First();
+				var firstChild = firstParent.Children.First();
+
+				// remove the first item
+				context.ChildThings.Remove(firstChild);
+
+				// add an item to the end
+				firstParent.Children.Add(new ChildThing
+				{
+					Name = "Added:"+DateTime.Now.ToString("G")
+				});
+
+				// save changes
+				context.SaveChanges();
+			}
+			return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+		}
+    }
+}
---
+++ TestApp/Models/TestModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Runtime.Serialization;
+
+namespace TestApp
+{
+	public class TestContextInitializer : DropCreateDatabaseIfModelChanges<TestEntities>
+	{
+		protected override void Seed(TestEntities context)
+		{
+			var parent = new ParentThing { Name = "TestParent" };
+			for (int j = 0; j < 3; j++)
+			{
+				parent.Children.Add(new ChildThing { 
+					Name = "Initial Item #"+j
+				});
+			}
+			context.ParentThings.Add(parent);
+		}
+	}
+
+	public class TestEntities : DbContext
+	{
+		public TestEntities() : base()
+		{
+			Database.SetInitializer<TestEntities>(new TestContextInitializer());
+		}
+
+		public DbSet<ParentThing> ParentThings { get; set; }
+		public DbSet<ChildThing> ChildThings { get; set; }
+	}
+
+	public class ParentThing
+	{
+		public ParentThing()
+		{
+			Children = new HashSet<ChildThing>();
+		}
+
+		[Key]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public virtual ICollection<ChildThing> Children { get; set; }
+	}
+
+	public class ChildThing
+	{
+		[Key]
+		public int ID { get; set; }
+		public int Parent_Id { get; set; }
+		public string Name { get; set; }
+
+		[IgnoreDataMember]
+		[ForeignKey("Parent_Id")]
+		public virtual ParentThing Parent { get; set; }
+	}
+}
